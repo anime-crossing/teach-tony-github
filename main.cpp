216e1bb2a6669e66ba546e7c86b02eabdb5dbe7c
Chai <ssumachai@gmail.com>
2022-05-13 15:53:37 -0700

    Lol hotfix

--- main.cpp
+++ main.cpp
@@ -19,6 +19,33 @@ int main(){
     // You should see it say: "modified: main.cpp"
     // Next, add, commit, and push your code to github
 
+    // Explaining "git add"
+        // Git can either add everything that has been changed...or one specific file
+        // "." is a term in CS that typically means "everything"
+            // "git add ." will add every single file to your "submission", but github is smart
+            // enough to only add the things that are changed
+        // If you want to add a specific file, you would do:
+            // "git add file-name.file_extension"
+            // To add "main.cpp", you would type "git add main.cpp"
+            // Chai will explain tabbing and autocomplete
+
+
+    //Explaining "git commit"
+        // Once you added everything you need to add, you essentially need to tell github
+        // Okay, I've confirmed the contents, it's ready to be put on the web
+        // type in "git commit -m "your message here""
+            // "git commit" is the commit command
+            // "-m" means that you have a message attached to your commit
+            // "your message here" is the message you wish to attach to your commit
+                // Usually. this message is sort of a progress indicator
+                // Chai will show examples
+        // So, a decent commit command may be:
+            // git commit -m "Added my name"
+        
+    //Explaining "git push"
+        // One of the most self-explanatory commands, "push" your changes to the live servers
+        // Once you push something, the code you just pushed will be on github
+    
     // Have Chai pull your code, and make the changes below:
 
     //Chai, type your favorite anime here in this line below:
@@ -29,5 +56,11 @@ int main(){
 
     // Chai will add, and push his code, and then you will pull his revisions
     
+    // Explaning "git pull"
+        // Much like "git push", that pushes your updated code to the internet, git pull checks
+        // to see if it was updated, and update your code to the most recent version if there is
+        // You will notice that your code will now have chai's favorite anime in line 53
+        
+
     return 0;
 }
